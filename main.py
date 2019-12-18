9def2afa27525fdb3fd5e5f002b94f33f4402162
runic-tears <51153795+runic-tears@users.noreply.github.com>
2019-12-18 14:56:26 +0400

    Python better tbh

---
+++ main.py
@@ -0,0 +1,5 @@
+from discord.ext import commands
+
+bot = commands.AutoShardedBot(command_prefix="dev?")
+
+bot.run("dsa")
--- src/events/guildDelete.js
+++
@@ -1,7 +0,0 @@
-const discord = require("discord.js");
-
-module.exports = async (client, guild) => {
-  console.log(`[GUILDS] Left ${guild.name} (${guild.id})`);
-
-  // TODO: Send embed with more info to logging guild/via webhook
-};
