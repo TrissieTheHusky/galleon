a350a3c35926eff1d52869b722eb7c4a4c10b756
runic-tears <51153795+runic-tears@users.noreply.github.com>
2019-12-20 22:58:19 +0400

    wow

--- src/cogs/Fun.py
+++ src/cogs/Fun.py
@@ -1,10 +1,69 @@
 from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import discord
+from os.path import join, dirname
+from PIL import Image, ImageDraw, ImageFont
+from functools import partial
+from io import BytesIO
+import textwrap
 import random
+import os
 
 
 class Fun(commands.Cog, name="Смешнявки"):
     def __init__(self, bot):
         self.bot = bot
+        self.meme_font = ImageFont.truetype(font=join(dirname(__file__), "../meme_templates/FiraMono-Bold.ttf"), size=72, encoding="utf-8")
+
+    def surprise_pika(self, text: str) -> BytesIO:
+        buffer = BytesIO()
+
+        with Image.open(join(dirname(__file__), "../meme_templates/pika_surprised.png")) as pika:
+            draw = ImageDraw.Draw(pika)
+
+            pika_width, pika_height = pika.size
+
+            char_width, char_height = self.meme_font.getsize('A')
+            chars_per_line = pika_width // char_width
+
+            text_line = textwrap.wrap(text.upper(), width=chars_per_line)
+
+            y = 20
+
+            for line in text_line:
+                line_width, line_height = self.meme_font.getsize(line)
+
+                x = (pika_width - line_width) / 2
+
+                # Обводка
+                draw.text((x-2, y), line, font=self.meme_font, fill='black')
+                draw.text((x+2, y), line, font=self.meme_font, fill='black')
+                draw.text((x, y-2), line, font=self.meme_font, fill='black')
+                draw.text((x, y+2), line, font=self.meme_font, fill='black')
+
+                # Сам текст
+                draw.text((x, y), line, fill='white', font=self.meme_font)
+
+                y += line_height
+
+            pika.save(buffer, format="png")
+
+        buffer.seek(0)
+
+        return buffer
+
+    @commands.command(name="pika")
+    @commands.cooldown(1, 30, BucketType.user)
+    async def surprised_pika(self, ctx, *, body: str):
+        """Генерирует мем с шкоированным Пикачу :о"""
+        msg = await ctx.send("Generating your HQ meme...")
+
+        fn = partial(self.surprise_pika, body)
+        meme = await self.bot.loop.run_in_executor(None, fn)
+        file = discord.File(fp=meme, filename="pika.png")
+
+        await ctx.send(file=file, content=f"{ctx.author.mention}, ваше чудо-творение готово!")
+        return await msg.delete()
 
     @commands.command()
     async def reverse_text(self, ctx, *, body: str):
