d027d38803df5ca4664dda319b56764352a0f9a8
defracted <51153795+defracted@users.noreply.github.com>
2020-05-04 14:30:35 +0400

    Reworked menus so they can be reusable

--- src/cogs/ToDo.py
+++ src/cogs/ToDo.py
@@ -1,48 +1,14 @@
-from discord import Embed
 from discord.ext import commands
-from discord.ext.menus import MenuPages, ListPageSource, button, First, Last
 
 from src.utils.custom_bot_class import DefraBot
 from src.utils.premade_embeds import warn_embed
 from src.utils.translator import Translator
+from src.utils.menus import MyPagesMenu, MyPagesSource
 
 
-class TodosMenu(MenuPages, inherit_buttons=False):
-    def __init__(self, source, **kwargs):
-        super().__init__(source, **kwargs)
-
-    @button('<:previous_page:706576101844975716>', position=First(1))
-    async def go_to_previous_page(self, payload):
-        """go to the previous page"""
-        await self.show_checked_page(self.current_page - 1)
-
-    @button('<:next_page:706576101719277629>', position=Last(2))
-    async def go_to_next_page(self, payload):
-        """go to the next page"""
-        await self.show_checked_page(self.current_page + 1)
-
-    @button('<:stop:706576101681528853>', position=Last(0))
-    async def stop_pages(self, payload):
-        """stops the pagination session."""
-        self.stop()
-
-
-class TodosSource(ListPageSource):
-    def __init__(self, entries, *, per_page, user):
-        super().__init__(entries, per_page=per_page)
-        self.embed = Embed(color=0x008081)
-        self.user = user
-
-    def format_page(self, menu, page):
-        if isinstance(page, str):
-            return page
-        else:
-            self.embed.set_author(name=Translator.translate("TODOS_LIST_TITLE", menu.ctx, user=str(self.user)),
-                                  icon_url=self.user.avatar_url)
-            self.embed.set_footer(text=Translator.translate("PAGE", menu.ctx) +
-                                       f" {menu.current_page + 1}/{self.get_max_pages()}")
-            self.embed.description = '\n'.join(page)
-            return self.embed
+class TodosSource(MyPagesSource):
+    def __init__(self, entries, *, title):
+        super().__init__(entries, per_page=4, title=title)
 
 
 class ToDo(commands.Cog):
@@ -72,8 +38,8 @@ class ToDo(commands.Cog):
                  ("..." if len(todo.get('content')) > 300 else "")) for todo in current_todos
             ]
 
-            source = TodosSource(entries, per_page=4, user=ctx.author)
-            menu = TodosMenu(source, delete_message_after=True)
+            source = TodosSource(entries, title=Translator.translate("TODOS_LIST_TITLE", ctx, user=ctx.author))
+            menu = MyPagesMenu(source, delete_message_after=True)
             await menu.start(ctx)
 
     @todo.command()
--- src/utils/menus.py
+++ src/utils/menus.py
@@ -1,37 +1,41 @@
-# TODO: make the util
-# class TodosMenu(MenuPages, inherit_buttons=False):
-#     def __init__(self, source, **kwargs):
-#         super().__init__(source, **kwargs)
-#
-#     @button('<:previous_page:706576101844975716>', position=First(1))
-#     async def go_to_previous_page(self, payload):
-#         """go to the previous page"""
-#         await self.show_checked_page(self.current_page - 1)
-#
-#     @button('<:next_page:706576101719277629>', position=Last(2))
-#     async def go_to_next_page(self, payload):
-#         """go to the next page"""
-#         await self.show_checked_page(self.current_page + 1)
-#
-#     @button('<:stop:706576101681528853>', position=Last(0))
-#     async def stop_pages(self, payload):
-#         """stops the pagination session."""
-#         self.stop()
-#
-#
-# class TodosSource(ListPageSource):
-#     def __init__(self, entries, *, per_page, user):
-#         super().__init__(entries, per_page=per_page)
-#         self.embed = Embed(color=0x008081)
-#         self.user = user
-#
-#     def format_page(self, menu, page):
-#         if isinstance(page, str):
-#             return page
-#         else:
-#             self.embed.set_author(name=Translator.translate("TODOS_LIST_TITLE", menu.ctx, user=str(self.user)),
-#                                   icon_url=self.user.avatar_url)
-#             self.embed.set_footer(text=Translator.translate("PAGE", menu.ctx) +
-#                                        f" {menu.current_page + 1}/{self.get_max_pages()}")
-#             self.embed.description = '\n'.join(page)
-#             return self.embed
+from discord.ext.menus import MenuPages, ListPageSource, button, First, Last
+from discord import Embed
+from src.utils.translator import Translator
+
+
+class MyPagesMenu(MenuPages, inherit_buttons=False):
+    def __init__(self, source, **kwargs):
+        super().__init__(source, **kwargs)
+
+    @button('<:previous_page:706576101844975716>', position=First(1))
+    async def go_to_previous_page(self, payload):
+        """go to the previous page"""
+        await self.show_checked_page(self.current_page - 1)
+
+    @button('<:next_page:706576101719277629>', position=Last(2))
+    async def go_to_next_page(self, payload):
+        """go to the next page"""
+        await self.show_checked_page(self.current_page + 1)
+
+    @button('<:stop:706576101681528853>', position=Last(0))
+    async def stop_pages(self, payload):
+        """stops the pagination session."""
+        self.stop()
+
+
+class MyPagesSource(ListPageSource):
+    def __init__(self, entries, *, per_page, title=None, **kwargs):
+        super().__init__(entries, per_page=per_page)
+        self.embed = Embed(color=0x008081)
+
+        if title is not None:
+            self.embed.title = title
+
+    def format_page(self, menu, page):
+        if isinstance(page, str):
+            return page
+        else:
+            self.embed.set_footer(text=Translator.translate("PAGE", menu.ctx) +
+                                       f" {menu.current_page + 1}/{self.get_max_pages()}")
+            self.embed.description = '\n'.join(page)
+            return self.embed
