b7d1e649023e32aa1261705eca7ac55f402ceb1d
defracted <51153795+defracted@users.noreply.github.com>
2020-05-07 13:58:40 +0400

    Simplified server manager check

--- src/utils/checks.py
+++ src/utils/checks.py
@@ -6,7 +6,7 @@ def is_server_manager_or_bot_owner():
     Checks if context author is a bot owner or has manage_guild permission
     """
 
-    def predicate(ctx: commands.Context):
-        return (ctx.author.id == ctx.bot.owner.id) or ((ctx.author.guild_permissions.value & 0x20) == 0x20)
+    def predicate(ctx):
+        return ctx.author.id == ctx.bot.owner.id or ctx.channel.permissions_for(ctx.author).manage_guild
 
     return commands.check(predicate)
