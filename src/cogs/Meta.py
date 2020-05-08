14ee0e86f2fce6ff4dd45fce87ac98e9db9e28c3
defracted <51153795+defracted@users.noreply.github.com>
2020-05-08 17:06:50 +0400

    avatarcolors belongs to meta, not fun

--- src/cogs/Meta.py
+++ src/cogs/Meta.py
@@ -1,7 +1,13 @@
+import textwrap
 from datetime import datetime
+from functools import partial
+from io import BytesIO
+from os.path import join, dirname
 from typing import Union
 
 import discord
+from PIL import Image, ImageDraw, ImageFont
+from colorthief import ColorThief
 from discord.ext import commands
 from pytz import timezone
 
@@ -17,6 +23,7 @@ from src.utils.translator import Translator
 class Meta(commands.Cog):
     def __init__(self, bot):
         self.bot: DefraBot = bot
+        self.pil_font = ImageFont.truetype(font=join(dirname(__file__), "../meme_templates/FiraSans-SemiBold.ttf"), size=30, encoding="utf-8")
         self.info_emojis = dict(
             staff="<:staff_badge:704986283008851975>", partner="<:partner_badge:704986283189075968>",
             hse="<:hypesquad_events_badge:704989654033629205>", bravery="<:bravery_badge:704986283231019079>",
@@ -36,6 +43,62 @@ class Meta(commands.Cog):
             return "<:dnd:705378474608558112>"
         elif status == discord.Status.offline:
             return "<:offline:705378474373677107>"
+
+    @commands.command(aliases=("ac", "avatarcolor"))
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def avatarcolors(self, ctx, user: Union[discord.Member, NotCachedUser] = None):
+        """AVATARCOLORS_HELP"""
+        await ctx.trigger_typing()
+
+        user = user or ctx.author
+
+        user_avatar_bytes = BytesIO(await user.avatar_url_as(format="png").read())
+        colors = ColorThief(user_avatar_bytes)
+        palette = colors.get_palette(quality=1, color_count=5)
+
+        def make_the_image():
+            buffer = BytesIO()
+
+            with Image.new('RGBA', (900, 600), (0, 0, 0, 0)) as im:
+
+                avatar = Image.open(user_avatar_bytes).resize((500, 500), Image.ANTIALIAS)
+                avatar.convert("RGBA")
+                im.paste(avatar, (300, 10))
+
+                img_width, img_height = avatar.size
+
+                char_width, char_height = self.pil_font.getsize('A')
+                chars_per_line = img_width // char_width
+
+                text_line = textwrap.wrap(f"{user.name}#{user.discriminator}", width=chars_per_line)
+
+                draw = ImageDraw.Draw(im)
+                for index, color in enumerate(palette):
+                    y = (index * 100) + 10
+                    draw.ellipse([(0, 0 + y), (100, 100 + y)], fill=color, outline=(0, 0, 0))
+
+                    r, g, b = color
+                    hex_color = "{:02x}{:02x}{:02x}".format(r, g, b)
+                    draw.text((110, 0 + (y + 31)), f"#{hex_color}", (255, 255, 255), font=self.pil_font)
+
+                for line in text_line:
+                    line_width, line_height = self.pil_font.getsize(line)
+                    x = 300 + (img_width - line_width) / 2
+                    y = 530
+
+                    draw.text((x, y), line, (255, 255, 255), font=self.pil_font)
+                    y += line_height
+
+                im.save(buffer, "png")
+
+            buffer.seek(0)
+            return buffer
+
+        fn = partial(make_the_image)
+        final_buffer = await self.bot.loop.run_in_executor(None, fn)
+        file = discord.File(filename=f"avatar_colors_for_{user.display_name}.png", fp=final_buffer)
+
+        await ctx.send(file=file)
 
     @commands.command(aliases=('tr',))
     async def translate(self, ctx, translate_from, translate_to, *, text: str):
