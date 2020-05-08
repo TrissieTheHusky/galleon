486ea86ad34c18250d831f0bdde6ceddc1194825
defracted <51153795+defracted@users.noreply.github.com>
2020-05-08 16:38:53 +0400

    Added avatarcolors command

--- src/cogs/Fun.py
+++ src/cogs/Fun.py
@@ -1,11 +1,18 @@
 import asyncio
 import random
-from typing import Optional
+import textwrap
+from functools import partial
+from io import BytesIO
+from os.path import join, dirname
+from typing import Optional, Union
 
-from discord import Status
+from PIL import Image, ImageDraw, ImageFont
+from colorthief import ColorThief
+from discord import Status, Member, File
 from discord.ext import commands
 
 from src.utils.base import is_num_in_str, text_from_bits, text_to_bits
+from src.utils.converters import NotCachedUser
 from src.utils.custom_bot_class import DefraBot
 from src.utils.jokes import Jokes
 from src.utils.premade_embeds import DefraEmbed
@@ -15,6 +22,63 @@ from src.utils.translator import Translator
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot: DefraBot = bot
+        self.pil_font = ImageFont.truetype(font=join(dirname(__file__), "../meme_templates/FiraSans-SemiBold.ttf"), size=30, encoding="utf-8")
+
+    @commands.command(aliases=("ac", "avatarcolor"))
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def avatarcolors(self, ctx, user: Optional[Union[Member, NotCachedUser]] = None):
+        """AVATARCOLORS_HELP"""
+        await ctx.trigger_typing()
+
+        user = user or ctx.author
+
+        user_avatar_bytes = BytesIO(await user.avatar_url_as(format="png").read())
+        colors = ColorThief(user_avatar_bytes)
+        palette = colors.get_palette(quality=1, color_count=5)
+
+        def make_the_image():
+            buffer = BytesIO()
+
+            with Image.new('RGBA', (900, 600), (0, 0, 0, 0)) as im:
+
+                avatar = Image.open(user_avatar_bytes).resize((500, 500), Image.ANTIALIAS)
+                avatar.convert("RGBA")
+                im.paste(avatar, (300, 10))
+
+                img_width, img_height = avatar.size
+
+                char_width, char_height = self.pil_font.getsize('A')
+                chars_per_line = img_width // char_width
+
+                text_line = textwrap.wrap(f"{user.name}#{user.discriminator}", width=chars_per_line)
+
+                draw = ImageDraw.Draw(im)
+                for index, color in enumerate(palette):
+                    y = (index * 100) + 10
+                    draw.ellipse([(0, 0 + y), (100, 100 + y)], fill=color, outline=(0, 0, 0))
+
+                    r, g, b = color
+                    hex_color = "{:02x}{:02x}{:02x}".format(r, g, b)
+                    draw.text((110, 0 + (y + 31)), f"#{hex_color}", (255, 255, 255), font=self.pil_font)
+
+                for line in text_line:
+                    line_width, line_height = self.pil_font.getsize(line)
+                    x = 300 + (img_width - line_width) / 2
+                    y = 530
+
+                    draw.text((x, y), line, (255, 255, 255), font=self.pil_font)
+                    y += line_height
+
+                im.save(buffer, "png")
+
+            buffer.seek(0)
+            return buffer
+
+        fn = partial(make_the_image)
+        final_buffer = await self.bot.loop.run_in_executor(None, fn)
+        file = File(filename=f"avatar_colors_for_{user.display_name}.png", fp=final_buffer)
+
+        await ctx.send(file=file)
 
     @commands.command()
     @commands.cooldown(2, 5, commands.BucketType.guild)
