431902468909dc6256b69b0838ad336da05e4ea4
defracted <51153795+defracted@users.noreply.github.com>
2020-05-09 15:37:09 +0400

    Copyright things

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Galleon — Discord Bot
+~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2020 defracted
+:license: MPL 2.0
+"""
+
+__title__ = "Galleon"
+__author__ = "defracted"
+__copyright__ = "Copyright 2020 defracted"
+__license__ = "MPL 2.0"
+__maintainer__ = "defracted"
+__email__ = "contact@defracted.net"
--- src/utils/cache.py
+++ src/utils/cache.py
@@ -1,3 +1,9 @@
+#  Copyright (c) 2020 defracted
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from typing import Dict, MutableSet
 
 from src.utils.database import Database
