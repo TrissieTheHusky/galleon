aaad1d0f89106d9a4b00af0e4c34ee098f7f6a11
defracted <51153795+defracted@users.noreply.github.com>
2020-05-18 22:37:52 +0400

    Unused import

--- src/utils/cache/__init__.py
+++ src/utils/cache/__init__.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from typing import Dict, MutableSet
+from typing import MutableSet
 
 from src.utils.database import Database
 from src.utils.logger import logger
